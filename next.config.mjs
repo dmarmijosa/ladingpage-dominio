5ed38311db0827a7188b3c8dd5fc3d566f65c07b
Danny Armijos <daniel2667@hotmail.es>
2025-07-21 02:31:29 +0200

    Add Kubernetes manifests and Docker configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: "standalone",
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
